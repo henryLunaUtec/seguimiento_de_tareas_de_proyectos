12e0e7676a670095ce5f03f71102451be299eb1c
Henry Luna <lunalandaverde23@gmail.com>
2026-02-17 20:56:43 -0600

    Agregar archivos de proyecto.

---
+++ About.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace seguimiento_de_tareas_de_proyectos
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
